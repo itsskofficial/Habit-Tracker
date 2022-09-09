ba053648c661bc49ba2a43508b325b36b9ae18c8
SK <sarthakkarandikar03@gmail.com>
2022-09-09 12:17:06 +0530

    added tk

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from turtle import title
 import requests
 import tkinter
 
+window=tkinter.Tk()
+window.mainloop()
 user_status=int(input("Press 0 for an existing user or 1 for a new one: "))
 if user_status==0:
     print()
