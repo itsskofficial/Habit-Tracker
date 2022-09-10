14c90dba8bd3a99ed55fde05ad8f0cd8dfe345fb
SK <sarthakkarandikar03@gmail.com>
2022-09-10 12:40:42 +0530

    nothing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import tkinter
 
 window=tkinter.Tk()
+window.config(h)
 window.mainloop()
 user_status=int(input("Press 0 for an existing user or 1 for a new one: "))
 if user_status==0:
