ea4e528d56a6dd75aca3b04f9be0897c4b512ad3
SK <sarthakkarandikar03@gmail.com>
2022-09-12 06:42:58 +0530

    added login & signup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.cmd import Command
 from turtle import onclick, title
 from xml.dom.minidom import Entity
 import requests
@@ -7,14 +8,59 @@ from functions import *
 window = tkinter.Tk()
 window.title("Habit Tracker")
 
+
+def Login():
+    pass
+
+def Signup():
+
+
 def onClickLogin():
     btn1.destroy()
     btn2.destroy()
     lbl1.config(text="Login")
-    usernameLabel=tkinter.Label(window,text="Username")
-    usernameEntry=tkinter.Entry(window)
-    usernameLabel.grid(row=1,column=1)
-    usernameEntry.grid(row=1,column=1)
+    lbl1.grid(columnspan=2)
+    usernameLabel = tkinter.Label(window, text="Username :", font=("Montserrat", 15))
+    usernameEntry = tkinter.Entry(window, font=("Montserrat", 15))
+    usernameLabel.grid(row=1, column=1, pady=20)
+    usernameEntry.grid(row=1, column=2, pady=20)
+    passwordLabel = tkinter.Label(window, text="Password :", font=("Montserrat", 15))
+    passwordEntry = tkinter.Entry(window, font=("Montserrat", 15))
+    passwordLabel.grid(row=2, column=1, pady=20)
+    passwordEntry.grid(row=2, column=2, pady=20)
+    loginButton = tkinter.Button(
+        window,
+        text="Login",
+        width=20,
+        height=3,
+        font=("Montserrat", 15),
+        command=Login,
+    )
+    loginButton.grid(row=3, column=1, columnspan=2, pady=20)
+
+def onClickSignup():
+    btn1.destroy()
+    btn2.destroy()
+    lbl1.config(text="Signup")
+    lbl1.grid(columnspan=2)
+    usernameLabel = tkinter.Label(window, text="Username :", font=("Montserrat", 15))
+    usernameEntry = tkinter.Entry(window, font=("Montserrat", 15))
+    usernameLabel.grid(row=1, column=1, pady=20)
+    usernameEntry.grid(row=1, column=2, pady=20)
+    passwordLabel = tkinter.Label(window, text="Password :", font=("Montserrat", 15))
+    passwordEntry = tkinter.Entry(window, font=("Montserrat", 15))
+    passwordLabel.grid(row=2, column=1, pady=20)
+    passwordEntry.grid(row=2, column=2, pady=20)
+    signupButton = tkinter.Button(
+        window,
+        text="Login",
+        width=20,
+        height=3,
+        font=("Montserrat", 15),
+        command=Signup,
+    )
+    signupButton.grid(row=3, column=1, columnspan=2, pady=20)
+
 
 # Create label
 lbl1 = tkinter.Label(
@@ -25,11 +71,22 @@ lbl1 = tkinter.Label(
     font=("Helvetica", 30),
 )
 lbl1.grid(row=0, column=1)
+
 # Create buttons
-btn1 = tkinter.Button(window, text="Login",width=20,height=3,font=("Montserrat",15),command=onClickLogin)
-btn2 = tkinter.Button(window, text="Signup", width=20, height=3,font=("Montserrat",15))
-btn1.grid(row=1, column=1, pady=10)
-btn2.grid(row=2, column=1, pady=10)
+
+btn1 = tkinter.Button(
+    window,
+    text="Login",
+    width=20,
+    height=3,
+    font=("Montserrat", 15),
+    command=onClickLogin,
+)
+btn2 = tkinter.Button(
+    window, text="Signup", width=20, height=3, font=("Montserrat", 15),command=onClickSignup
+)
+btn1.grid(row=1, column=1, pady=20)
+btn2.grid(row=2, column=1, pady=20)
 
 
 window.mainloop()
